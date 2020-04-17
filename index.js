d74f425fb57ab150238b32720e7e3d94731213e2
Unknown <dianairisrf1996@gmail.com>
2020-04-17 16:40:20 -0600

    Orden

--- index.js
+++ index.js
@@ -1,45 +1,36 @@
 const express = require('express');
 const app = express();
-app.set("port", 8000);
+
+
+
+////################TEMPLATES################
 app.engine('html',require('ejs').renderFile);
 app.set('view engine', 'ejs');
 
-//Middleware
+
+//################MIDDLEWARE################
 const bodyParser = require('body-parser');
-
-
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
 //Express utilizar el json con tipo application/json con el texto plano
 //Se utiliza para eliminar, debido a que node creia que se le estaba pasando un txt plano
 app.use(express.json({
   type: ['application/json', 'text/plain']
 }));
-
 app.use(require("body-parser").json());
 
 
-
+//################RUTAS################
 app.use(require('./Rutas/index'));
+
+
+//################ARCHIVOS_STATICOS################
 app.use(express.static(__dirname + '/public'));
 
 
+//################PUERTO_SERVIDOR_WEB################
+app.set("port", 8000);
 app.listen(app.get("port"), () => {
   console.log("server en el puerto", app.get("port"));
 });
 
-// //Conexión con la base de datos
-// //Colección de Bodegas
-// const MongoClient = require('mongodb').MongoClient;
-// const uri = "mongodb+srv://diseno:Ulacit1234@cluster0-40do9.mongodb.net/test?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: false });
-// client.connect(err => {
-//   const collection = client.db("tramsadb").collection("bodega");
-//   collection.find({}).toArray(function (err, result) {
-//     if (err) throw err;
-//     console.log(result);
-//     client.close();
-// });
-// });
 
